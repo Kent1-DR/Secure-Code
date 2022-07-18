3f890a5af33204fb71222f33042af47f88344182
Quentin_drch <105045079+Kent1-DR@users.noreply.github.com>
2022-07-18 10:16:24 +0200

    Delete Set the time privilege secure code.py

--- projetsc/Set the time privilege secure code.py
+++
@@ -1,9 +0,0 @@
-#how to set the time...
-
-from datetime import datetime
-from datetime import date 
-date = date.today()
-print ('Follow the steps to set the time :')
-
-#how to ask the user for parameters ?
-newdate = date.replace()
